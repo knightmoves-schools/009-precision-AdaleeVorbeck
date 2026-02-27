90f51e3623b026c86e5c6ef7f58627a3fed5554e
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-02-27 18:24:43 +0000

    WE SANG FOR OUR SUPPERS

--- Precision.cs
+++ Precision.cs
@@ -2,5 +2,10 @@
 public class Precision
 {
     // Add your code here
-    
+    public int MaxInteger = 2147483647;
+    public double DecimalProduct = 2.53468 * 5.75956733;
+
 }
+
+//should assign 2,147,483,647 to a class-level variable named MaxInteger
+//should assign the product of 2.53468 multiplied by 5.75956733 to a class-level variable named DecimalProduct
